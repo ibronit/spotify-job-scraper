a0a7c83f1ec72815d659c402460280de6f7396eb
Norbert Parraghy <norbert.parraghy@gmail.com>
2020-02-09 21:50:59 +0100

    remove unnecessary TODOS

--- src/Service/JobScraper.php
+++ src/Service/JobScraper.php
@@ -23,17 +23,13 @@ class JobScraper
      */
     public function scrapeJob(Job $job): Job
     {
-        // TODO: check if it returns 200
         $crawler = $this->client->request('GET', $job->getUrl());
-        try {
-            $jobCategory = $crawler->filter('html div.single-post--meta')->html();
-            $jobDescription = $crawler->filter('html div.column-inner')->html();
 
-            $job->setCategory(trim(strip_tags($jobCategory)));
-            $job->setDescription(trim(strip_tags($jobDescription)));
-        } catch (\InvalidArgumentException $e) {
-            // TODO: log error
-        }
+        $jobCategory = $crawler->filter('html div.single-post--meta')->html();
+        $jobDescription = $crawler->filter('html div.column-inner')->html();
+
+        $job->setCategory(trim(strip_tags($jobCategory)));
+        $job->setDescription(trim(strip_tags($jobDescription)));
 
         return $job;
     }
